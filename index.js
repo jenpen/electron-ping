2393eb139a0b4e8dd06dd361f4a3317121088759
jen pengelly <jgpengelly@gmail.com>
2017-08-23 21:22:57 -0400

    add notifications on click

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const {app, Menu, Tray, nativeImage} = require('electron')
+const notify = require('electron-main-notification')
 
 let tray = null
 const image = nativeImage.createFromPath('tray_icon_purple.png')
@@ -12,18 +13,21 @@ app.on('ready', () => {
     {label: 'Item2', type: 'radio'},
     {
       label: 'Say Hey',
-      click () { console.log('heyo')}
+      click () {
+        notify('Hey!', { body: 'How you doin?' }, () => {
+          console.log('The notification got clicked on!')
+        })
+      }
     },
     {role: 'quit'}
-
 
   ])
   tray.setToolTip('This is my application.')
   tray.setContextMenu(contextMenu)
-  tray.on('mouse-enter', ()=>{
+  tray.on('mouse-enter', () => {
     tray.setImage('tray_icon_purple.png')
   })
-  tray.on('mouse-leave', ()=>{
+  tray.on('mouse-leave', () => {
     tray.setImage(image)
   })
 })
