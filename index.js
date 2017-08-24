677fab1766a9a8c62e3d8fc3d7bdd38212817105
jen pengelly <jgpengelly@gmail.com>
2017-08-23 21:13:23 -0400

    make it work on dark mode

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const {app, Menu, Tray} = require('electron')
+const {app, Menu, Tray, nativeImage} = require('electron')
 
 let tray = null
+const image = nativeImage.createFromPath('tray_icon_purple.png')
+image.setTemplateImage(true)
+
 app.on('ready', () => {
   app.dock.hide()
-  tray = new Tray('tray_icon_black.png')
+  tray = new Tray(image)
   const contextMenu = Menu.buildFromTemplate([
     {label: 'Item1', type: 'radio', checked: true},
     {label: 'Item2', type: 'radio'},
@@ -21,6 +24,6 @@ app.on('ready', () => {
     tray.setImage('tray_icon_purple.png')
   })
   tray.on('mouse-leave', ()=>{
-    tray.setImage('tray_icon_black.png')
+    tray.setImage(image)
   })
 })
